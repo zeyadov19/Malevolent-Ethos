4c7da28814b0e6accde72539264e6f48f4e9cebf
ZeyadAlghannam <zeyadalghannam@gmail.com>
2025-07-02 10:45:56 +0300

    Main Menu Done

---
+++ Assets/DebuggingUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+    }
+}
