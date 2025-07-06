6cf77fae0a067e98a3eb8900bafde4415559e30c
ZeyadAlghannam <zeyadalghannam@gmail.com>
2025-07-06 15:06:46 +0300

    Added BackGround Follow to Map1

--- Assets/LoadingScreenScript.cs
+++ Assets/LoadingScreenScript.cs
@@ -12,6 +12,6 @@ public class SimpleCountdown : MonoBehaviour
     IEnumerator LoadSceneAfterDelay()
     {
         yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(2);
+        SceneManager.LoadSceneAsync(2);
     }
 }
